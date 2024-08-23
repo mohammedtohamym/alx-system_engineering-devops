c365bc1da0a9220e8ffb795e5279e6a9e0aa31c5
Mohammed Tohamy <125455529+mohammedtohamym@users.noreply.github.com>
2024-08-23 20:00:39 +0300

    task done

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,10 +1,7 @@
 # puppet file to increase number of open files for a user
 
-file { '/etc/security/limits.conf':
-  ensure => present,
-  content => [
-    'holberton hard nofile 60000',
-    'holberton soft nofile 20000',
-  ],
-  mode   => '0644',
+file { 'loginFile':
+    ensure  => present,
+    path    => '/etc/security/limits.conf',
+    content => '#File erased'
 }
